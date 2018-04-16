ea6eca24784db5909852a06fb590a07b8267d82b
Yundo Han <han@yun.do>
2018-04-16 17:42:41 +0900

    fix broken pathname of controllers and views

--- app/controllers/submits_controller.rb
+++ app/controllers/submits_controller.rb
@@ -1,6 +1,6 @@
-class SubmitController < ApplicationController
+class SubmitsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @submits = Submit.all.order('created_at desc')
     @submits_count = current_user.submits.length
@@ -11,12 +11,12 @@ class SubmitController < ApplicationController
 
   def create
     new_submit = Submit.new(user_id: current_user.id, content: params[:content])
-  
+
     if new_submit.save
       redirect_to submits_path
     else
       redirect_to new_submit_path
     end
   end
-  
+
 end
