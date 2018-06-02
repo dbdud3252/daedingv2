15b382f435bc0f891cda62b658fc0ccc6d20cfea
Youyoung Lee <dbdud3252@naver.com>
2018-06-03 02:41:41 +0900

    start frontend

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require materialize-sprockets
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery3
+//= require popper
+//= require bootstrap
